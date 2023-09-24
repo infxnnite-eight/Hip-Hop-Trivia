8565645f744ff6bb640dedd471842605b21f9a58
June238025$ <dorianquintana@gmail.com>
2023-09-24 01:47:42 -0700

    first-question-complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answersButtonsElement = document.getElementById('answer-buttons');
@@ -17,26 +18,57 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    clearStatusClass(questionContainerElement);
     showQuestion(shuffledQuestions[currentQuestionIndex]);
+    nextButton.classList.add('hide');
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
-    // You can add code here to display answer buttons based on the question's answers array.
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answersButtonsElement.appendChild(button);
+    });
 }
 
-function selectAnswer() {
-    // You can add code here to handle the user's answer selection.
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(selectedButton, correct);
+    Array.from(answersButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    nextButton.classList.remove('hide');
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
 const questions = [
     {
         question: 'What Rapper is considered the King Of The Of South ',
         answers: [
-            { text: 'Gucci Mane', correct: true },
+            { text: 'Gucci Mane', correct: false },
             { text: 'Denzel Curry', correct: false },
             { text: 'Andre 3000', correct: false },
-            { text: 'T.I.', correct: false },
+            { text: 'T.I.', correct: true },
         ]
     }
 ];
